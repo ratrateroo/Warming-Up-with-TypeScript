85df97532f6524495ca834426b6537832b66d70a
Lei <ratrateroo@gmail.com>
2021-01-07 22:46:03 +0800

    used tsc --init to use tsconfig.json file

---
+++ obj-arr-enum.js
@@ -0,0 +1,40 @@
+"use strict";
+// const person: {
+// 	name: string;
+// 	age: number;
+// } = {
+// const person: {
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	role: [number, string];
+// } = {
+// 	name: 'Marga',
+// 	age: 15,
+// 	hobbies: ['Reading', 'Dancing'],
+// 	role: [2, 'author'],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'Marga',
+    age: 15,
+    hobbies: ['Reading', 'Dancing'],
+    role: Role.ADMIN,
+};
+var activities;
+activities = ['Reading', 'Dancing'];
+var favoriteActivities;
+favoriteActivities = ['Reading', 'Dancing'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log('Is Author');
+}
