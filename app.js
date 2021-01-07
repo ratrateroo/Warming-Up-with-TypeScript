8ef02bb3d55e764a9aab076658cbda9a68a04e35
Lei <ratrateroo@gmail.com>
2021-01-07 22:28:36 +0800

    used the type never

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ userInput = 'Ara';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured', 500);
