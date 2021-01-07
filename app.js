4e9f0a5150e93e16fe0d795d7c46cd84b74f6141
Lei <ratrateroo@gmail.com>
2021-01-07 22:22:00 +0800

    used the unknown type

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Ara';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-console.log(combineValues(8, 8));
