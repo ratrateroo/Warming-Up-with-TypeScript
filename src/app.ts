38c97d32fa0e41007b08a25a849d3737a5b2eda4
Lei <ratrateroo@gmail.com>
2021-01-20 17:09:45 +0800

    added alternative code for the map

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,33 @@ import { ProjectList } from './components/project-list';
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
+const form = document.getElementById('search-form')!;
+const addressInput = document.getElementById('address') as HTMLInputElement;
+
+// function seachAddressHandler(event: Event) {
+// 	event.preventDefault();
+// 	const enteredAddress = addressInput.value;
+// }
+
+declare var ol: any;
+
+function searchAddressHandler(event: Event) {
+	event.preventDefault();
+
+	const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
+
+	document.getElementById('map')!.innerHTML = ''; // clear <p> from <div id="map">
+	new ol.Map({
+		target: 'map',
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM(),
+			}),
+		],
+		view: new ol.View({
+			center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+			zoom: 16,
+		}),
+	});
+}
