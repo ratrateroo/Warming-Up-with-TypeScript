c4aa7fb8859bff159049f1e040b127e4779217a9
Lei <ratrateroo@gmail.com>
2021-01-20 21:28:43 +0800

    added todo app components

--- react-typescript/my-app/src/App.tsx
+++ react-typescript/my-app/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App: React.FC = () => {
+	const todos = [{ id: 't1', text: 'Finish the course.' }];
+	return (
+		<div className="App">
+			<NewTodo />
+			<TodoList items={todos} />
+		</div>
+	);
+};
 
 export default App;
