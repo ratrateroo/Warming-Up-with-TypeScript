baeb373afc19a9694a96700555f975885c0f7887
Lei <ratrateroo@gmail.com>
2021-01-21 22:23:12 +0800

    added updated and delete functionality

--- nodejs-typescript/src/routes/todos.ts
+++ nodejs-typescript/src/routes/todos.ts
@@ -3,16 +3,21 @@ import { Router } from 'express';
 //const express = require('express');
 //const Router = express.Router;
 
-import { createTodo } from '../controllers/todos';
+import {
+	createTodo,
+	getTodos,
+	updateTodo,
+	deleteTodo,
+} from '../controllers/todos';
 
 const router = Router();
 
 router.post('/', createTodo);
 
-router.get('/');
+router.get('/', getTodos);
 
-router.patch('/:id');
+router.patch('/:id', updateTodo);
 
-router.delete('/:id');
+router.delete('/:id', deleteTodo);
 
 export default router;
